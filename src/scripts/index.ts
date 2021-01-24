be0f748dc70e1e2d645d681a29a8c3b5fa1ce475
clark-cui <cuirongchuan@163.com>
2021-01-24 11:34:47 +0800

    项目结构调整

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,6 @@
-import printMe from "./utils/test"
-import cardFirst from "./assets/covid-19.png"
-import cardSecond from "./assets/python.png"
+import printMe from "../utils/test"
+import cardFirst from "../assets/covid-19.png"
+import cardSecond from "../assets/python.png"
 
 const imgFirst:any=document.querySelector(".imgFirst");
 const imgSecond:any=document.querySelector(".imgSecond");
