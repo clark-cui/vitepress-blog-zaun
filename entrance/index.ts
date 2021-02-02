294d3c854dd80ddf2274818a5b56e114c4ae51a8
clark-cui <cuirongchuan@163.com>
2021-02-02 18:32:04 +0800

    rewrite dev

--- entrance/index.ts
+++
@@ -1,9 +0,0 @@
-import printMe from "../src/scripts/test"
-import cardFirst from "../assets/covid-19.png"
-import cardSecond from "../assets/python.png"
-
-const imgFirst:any=document.querySelector(".imgFirst");
-const imgSecond:any=document.querySelector(".imgSecond");
-imgFirst.src=cardFirst;
-imgSecond.src=cardSecond;
-printMe();
--- src/scripts/test.ts
+++
@@ -1,6 +0,0 @@
-import github_img_src from '../assets/github.svg';
-
-export default function () {
-  const github_img: any = document.querySelector('#github');
-  github_img.src = github_img_src;
-}
