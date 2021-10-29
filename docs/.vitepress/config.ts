5526ed0b256c337336152d5d06ebc2c9a2fd72e8
clark-cui <cuirongchuan@163.com>
2021-10-29 18:25:51 +0800

    reset head

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,67 @@ export default {
   // lang: 'en-US',
   title: "Clark Cui",
   description: "Home of Clark Cui",
-
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        // type: 'image/png',
+        type: "image/jpeg",
+        href: "/avator.jpg",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "author",
+        content: "Clark Cui",
+      },
+    ],
+    [
+      "meta",
+      {
+        property: "og:title",
+        content: "Home",
+      },
+    ],
+    // ['meta', {
+    //   property: 'og:image',
+    //   content: 'https://sli.dev/og-image.png'
+    // }],
+    [
+      "meta",
+      {
+        property: "og:description",
+        content: "Home of Clark Cui",
+      },
+    ],
+    // ['meta', {
+    //   name: 'twitter:card',
+    //   content: 'summary_large_image'
+    // }],
+    // ['meta', {
+    //   name: 'twitter:creator',
+    //   content: '@qingshuihe1'
+    // }],
+    // ['meta', {
+    //   name: 'twitter:image',
+    //   content: 'https://sli.dev/og-image.png'
+    // }],
+    // ['link', {
+    //   rel: 'dns-prefetch',
+    //   href: 'https://fonts.gstatic.com'
+    // }],
+    // ['link', {
+    //   rel: 'preconnect',
+    //   crossorigin: 'anonymous',
+    //   href: 'https://fonts.gstatic.com'
+    // }],
+    // ['link', {
+    //   href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@200;400;500&family=Inter:wght@200;400;500;600',
+    //   rel: 'stylesheet'
+    // }],
+  ],
   themeConfig: {
     repo: "clark-cui/homeSite",
     logo: "/tea.svg",
