6a520882f8fecd33fd845a0b48c6b4489f12c321
clark-cui <cuirongchuan@163.com>
2021-11-01 16:53:38 +0800

    add lottie

--- docs/.vitepress/theme/components/HomeHero.vue
+++ docs/.vitepress/theme/components/HomeHero.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="pic">
-    <picture>
-      <img :src="avator" alt="" class="src" />
-    </picture>
+    <div id="svgContainer"></div>
   </div>
 </template>
 <script lang="ts" setup>
 import avator from "../../../public/avator.jpg";
+import { onMounted } from "vue";
+import lottie from "lottie-web";
+onMounted(() => {
+  const svgContainer = document.getElementById("svgContainer");
+  const animItem = lottie.loadAnimation({
+    container: svgContainer,
+    renderer: "svg",
+    loop: true,
+    path: "https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json",
+  });
+});
 </script>
 <style scoped>
 .pic {
   text-align: center;
+}
+#svgContainer {
+  width: 300px;
+  margin: 0 auto;
 }
 </style>
