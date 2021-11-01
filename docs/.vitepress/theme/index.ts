4083ea790735c13e2a76265940eb8576145a3f34
clark-cui <cuirongchuan@163.com>
2021-11-01 16:40:13 +0800

    refector copywright

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,4 @@
 import DefaultTheme from "vitepress/theme";
-import Home from "./components/Home.vue";
 // import Docs from "./components/Docs.vue";
 // import Tags from "./components/Tags.vue";
 import MyLayout from "./components/MyLayout.vue";
@@ -9,10 +8,8 @@ export default {
   Layout: MyLayout,
   enhanceApp({ app, router, siteData }) {
     // register global components
-    app.component("Home", Home);
     // app.component("Docs", Docs);
     // app.component("Tags", Tags);
-
     // app is the Vue 3 app instance from createApp()
     // router is VitePress' custom router (see `lib/app/router.js`)
     // siteData is a ref of current site-level metadata.
