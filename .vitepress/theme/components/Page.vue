f445db149397cd22167c94012473b7819632ba8b
clark-cui <cuirongchuan@163.com>
2022-01-09 22:54:04 +0800

    fix: pagination auto

--- .vitepress/theme/components/Page.vue
+++ .vitepress/theme/components/Page.vue
@@ -6,14 +6,15 @@
     </a>
   </div>
   <div class="pagination">
-    <a
+    <div
       class="link"
-      :class="{ active: pageCurrent === i }"
+      :class="{ activeLink: pageCurrent === i }"
       v-for="i in pagesNum"
       :key="i"
       @click="go(i)"
-      >{{ i }}</a
     >
+      {{ i }}
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
@@ -39,7 +40,7 @@ let pagesNum =
     : postLength / pageSize + 1;
 pagesNum = parseInt(pagesNum.toString());
 //pageCurrent
-let pageCurrent = 0;
+let pageCurrent = ref(1);
 // filter index post
 postsAll = postsAll.filter((item: post) => {
   return item.regularPath.indexOf("index") < 0;
@@ -51,18 +52,20 @@ for (let i = 0; i < pagesNum; i++) {
 }
 let index = 0;
 for (let i = 0; i < postsAll.length; i++) {
-  if (allMap[index].length > 1) {
+  if (allMap[index].length > pageSize - 1) {
     index += 1;
   }
+  console.log(i, index, allMap, postsAll, "xx");
   allMap[index].push(postsAll[i]);
 }
-console.log(allMap, "allMap");
+// set posts
 let posts = ref([]);
-posts.value = allMap[pageCurrent];
+posts.value = allMap[pageCurrent.value - 1];
 
+// click pagination
 const go = (i) => {
-  pageCurrent = i;
-  posts.value = allMap[pageCurrent - 1];
+  pageCurrent.value = i;
+  posts.value = allMap[pageCurrent.value - 1];
 };
 // timestamp transform
 const transDate = (date: string) => {
@@ -131,10 +134,12 @@ onMounted(() => {
   margin: 10px;
   background: #fff;
   max-width: 600px;
-
   box-shadow: 6px 6px #646cff;
   border: 4px solid #282936;
   cursor: pointer;
+}
+.blog:hover {
+  text-decoration: none;
 }
 .title {
   color: #747bff;
@@ -144,88 +149,25 @@ onMounted(() => {
 .date {
   padding-bottom: 7px;
 }
-.list {
-  border-bottom: 1px dashed var(--c-divider-light);
-  padding: 1rem 0 0 0;
-}
-.list-header {
+.pagination {
   display: flex;
   align-items: center;
-  justify-content: space-between;
-}
-.list-li {
-  font-size: 1.0625rem;
-  font-weight: 500;
-  margin: 0.1rem 0;
-}
-
-.describe {
-  font-size: 0.9375rem;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
-  color: #71717a;
-  margin: 0.625rem 0 1rem;
-  line-height: 1.5rem;
-}
-.pagination {
-  margin-top: 16px;
-  display: flex;
   justify-content: center;
 }
 .link {
-  display: inline-block;
-  width: 28px;
-  height: 28px;
+  width: 1.5rem;
+  height: 1.5rem;
+  line-height: 1.5rem;
   text-align: center;
-  line-height: 28px;
-  border: 1px var(--c-divider-light) solid;
+  border: 1px solid #282936;
+  cursor: pointer;
   border-right: none;
 }
-.link.active {
-  background: var(--c-brand);
-  color: #fff;
-  border: 1px solid var(--c-brand) !important;
-}
-.link:first-child {
-  border-bottom-left-radius: 3px;
-  border-top-left-radius: 3px;
-}
 .link:last-child {
-  border-bottom-right-radius: 3px;
-  border-top-right-radius: 3px;
-  border-right: 1px var(--c-divider-light) solid;
+  border-right: 1px solid #282936;
 }
-
-@media screen and (max-width: 720px) {
-  .list {
-    padding: 1rem 0 0 0;
-  }
-  .list-header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .list-li {
-    font-size: 1.125rem;
-    font-weight: 400;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    overflow: hidden;
-    width: 17rem;
-  }
-  .describe {
-    font-size: 0.9375rem;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 3;
-    overflow: hidden;
-    margin: 0.5rem 0 1rem;
-  }
-  .date {
-    font-size: 0.8125rem;
-  }
+.activeLink {
+  background-color: #646cff;
+  color: white;
 }
 </style>
