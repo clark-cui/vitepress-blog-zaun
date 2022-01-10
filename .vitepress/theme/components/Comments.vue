dd3deafe08555ada12c32762283895902a57a176
clark-cui <cuirongchuan@163.com>
2022-01-10 21:11:09 +0800

    fix: comment add relativePath

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -5,20 +5,23 @@
 import "gitalk/dist/gitalk.css";
 import Gitalk from "gitalk";
 import { onMounted } from "vue";
-
-const gitalk = new Gitalk({
-  clientID: "94f8f287e970c48e2122",
-  clientSecret: "2a3c7be462ddf835cff98c4ffa7add4ba229ad7e",
-  repo: "blog-comments",
-  owner: "clark-cui",
-  admin: ["clark-cui"],
-  id: decodeURI(window.location.pathname), // Ensure uniqueness and length less than 50
-  language: "zh-CN",
-  distractionFreeMode: true, // Facebook-like distraction free mode
-});
-onMounted(() => {
-  gitalk.render("gitalk-container");
-});
+import { useData } from "vitepress";
+const relativePath = useData().page.value.relativePath;
+if (relativePath.indexOf("posts") > -1) {
+  const gitalk = new Gitalk({
+    clientID: "94f8f287e970c48e2122",
+    clientSecret: "2a3c7be462ddf835cff98c4ffa7add4ba229ad7e",
+    repo: "blog-comments",
+    owner: "clark-cui",
+    admin: ["clark-cui"],
+    id: decodeURI(window.location.pathname), // Ensure uniqueness and length less than 50
+    language: "zh-CN",
+    distractionFreeMode: true, // Facebook-like distraction free mode
+  });
+  onMounted(() => {
+    gitalk.render("gitalk-container");
+  });
+}
 </script>
 <style scoped>
 </style>
