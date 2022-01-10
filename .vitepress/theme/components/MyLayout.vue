9e2d0d8f860ebcd45a431ff6535d74708ad3b9d0
clark-cui <cuirongchuan@163.com>
2022-01-10 19:45:16 +0800

    feature: add gitalk

--- .vitepress/theme/components/MyLayout.vue
+++ .vitepress/theme/components/MyLayout.vue
@@ -4,6 +4,9 @@
     <template #page-top>
       <Category />
     </template>
+    <template #page-bottom>
+      <div id="gitalk-container"></div>
+    </template>
     <!-- Home slot-->
     <template #home-hero><HomeHero /> </template>
     <template #home-features> <Page /></template>
@@ -13,12 +16,23 @@
 </template>
 <script lang="ts" setup>
 import DefaultTheme from "vitepress/theme";
+import Gitalk from "gitalk";
 const { Layout } = DefaultTheme;
 import HomeHero from "./HomeHero.vue";
 import CopyWright from "./CopyWright.vue";
 import Page from "./Page.vue";
 import ToggleTheme from "./ToggleTheme.vue";
 import Category from "./Category.vue";
+const gitalk = new Gitalk({
+  clientID: "94f8f287e970c48e2122",
+  clientSecret: "2a3c7be462ddf835cff98c4ffa7add4ba229ad7e",
+  repo: "blog-comment",
+  owner: "clark-cui",
+  admin: ["clark-cui"],
+  id: decodeURI(window.location.pathname), // Ensure uniqueness and length less than 50
+  language: "zh-CN",
+  distractionFreeMode: true, // Facebook-like distraction free mode
+});
 </script>
 <style scoped>
 </style>
