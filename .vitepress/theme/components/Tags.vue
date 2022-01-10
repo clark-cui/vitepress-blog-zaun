f4ed5f2b80fd81a107d3c622f5c9f9763ef5b001
clark-cui <cuirongchuan@163.com>
2022-01-10 11:23:05 +0800

    enhance(tag): style enhancement

--- .vitepress/theme/components/Tags.vue
+++ .vitepress/theme/components/Tags.vue
@@ -1,10 +1,35 @@
 <template>
+  <h1 class="tags-header">Tags</h1>
   <div class="tags">
-    <span @click="toggleTag(key)" v-for="(item, key) in data" class="tag">
-      {{ key }} <strong>{{ data[key].length }}</strong>
+    <span
+      @click="toggleTag(key)"
+      v-for="(item, key) in data"
+      class="tag"
+      :style="getFontSize(data[key].length)"
+      :class="{ activetag: selectTag === key }"
+    >
+      {{ key }} <span class="tag-length">{{ data[key].length }}</span>
     </span>
   </div>
-  <div class="header">{{ selectTag }}</div>
+
+  <h4 class="header" v-show="selectTag">
+    <svg
+      t="1641783753540"
+      class="fas-icon"
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      p-id="1254"
+      width="200"
+      height="200"
+    >
+      <path
+        d="M995.126867 592.38l-360.08 360.08a53.333333 53.333333 0 0 1-71.333334 3.68l356.22-356.22a64 64 0 0 0 0-90.506667L495.8402 85.333333h45.573333a52.986667 52.986667 0 0 1 37.713334 15.62l416 416a53.4 53.4 0 0 1 0 75.426667z m-128 0l-360.08 360.08a53.333333 53.333333 0 0 1-75.426667 0l-416-416A52.986667 52.986667 0 0 1 0.0002 498.746667V138.666667a53.393333 53.393333 0 0 1 53.333333-53.333334h360.08a52.986667 52.986667 0 0 1 37.713334 15.62l416 416a53.4 53.4 0 0 1 0 75.426667zM341.333533 341.333333a85.333333 85.333333 0 1 0-85.333333 85.333334 85.426667 85.426667 0 0 0 85.333333-85.333334z"
+        fill="#646cff"
+        p-id="1255"
+      ></path></svg
+    ><span class="header-text">{{ selectTag }}</span>
+  </h4>
   <a
     :href="withBase(article.regularPath)"
     v-for="(article, index) in data[selectTag]"
@@ -25,53 +50,83 @@ import { initTags } from "../utils";
 
 const { theme } = useData();
 const data = computed(() => initTags(theme.value.posts));
+console.log(data, "data");
 let selectTag = ref("");
 const toggleTag = (tag: string) => {
   selectTag.value = tag;
 };
+// set font-size
+const getFontSize = (length: number) => {
+  let size = length * 0.04 + 0.85;
+  return { fontSize: `${size}em` };
+};
 </script>
 
 <style scoped>
+.tags-header {
+  font-weight: bold;
+  padding-bottom: 14px;
+}
 .tags {
   margin-top: 14px;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  justify-content: center;
+  justify-content: left;
+
+  border-bottom: 1px dashed #c7c7c7;
+  margin-bottom: 10px;
+  padding-bottom: 20px;
 }
 .tag {
   display: inline-block;
-  padding: 4px 16px;
   margin: 6px 8px;
-  font-size: 0.875rem;
+  font-size: 0.85em;
   line-height: 25px;
-  background-color: var(--tag-bg);
   transition: 0.4s;
-  border-radius: 3px;
-  color: var(--c-brand);
+  color: #a1a1a1;
   cursor: pointer;
 }
-.tag strong {
-  color: #222;
+.tag:hover {
+  color: #212121;
+}
+.activetag {
+  color: #212121;
+}
+.tag-length {
+  color: #646cff;
+  font-size: 12px !important;
+  position: relative;
+  top: -8px;
 }
 .header {
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: 600;
-  margin: 1rem 0;
-  text-align: center;
+  margin: 1.5rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+}
+.fas-icon {
+  width: 2rem;
+  height: 2rem;
+}
+.header-text {
+  padding-left: 10px;
 }
 .article {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 5px 10px;
+  margin: 10px 10px;
+
+  border-bottom: 2px solid #646cff;
+  color: #666;
+  transition: border 0.3s ease, color 0.3s ease;
 }
-@media screen and (max-width: 700px) {
-  .header {
-    font-size: 1.5rem;
-  }
-  .date {
-    font-size: 0.75rem;
-  }
+.article:hover {
+  text-decoration: none;
+  color: #212121;
+  border-color: #212121;
 }
 </style>
