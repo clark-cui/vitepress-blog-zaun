a27fa2b4a15042936c3aebe4ae4052017a26f156
clark-cui <cuirongchuan@163.com>
2022-01-10 20:19:23 +0800

    fix: repo error

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -9,7 +9,7 @@ import { onMounted } from "vue";
 const gitalk = new Gitalk({
   clientID: "94f8f287e970c48e2122",
   clientSecret: "2a3c7be462ddf835cff98c4ffa7add4ba229ad7e",
-  repo: "blog-comment",
+  repo: "blog-comments",
   owner: "clark-cui",
   admin: ["clark-cui"],
   id: decodeURI(window.location.pathname), // Ensure uniqueness and length less than 50
