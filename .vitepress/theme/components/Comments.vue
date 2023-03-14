ec053dfa4db6b35edd7aabdc629ab61e2ce47c64
clark-cui <cuirongchuan@163.com>
2023-03-14 23:19:44 +0800

    feat: update deps && fix ssr hydrate location issue

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -8,21 +8,18 @@ import { onMounted } from "vue";
 // import { useData } from "vitepress";
 // const relativePath = useData().page.value.relativePath;
 
-const gitalk = new Gitalk({
-  clientID: "a8430bf8a0464113ee24",
-  clientSecret: "df4cbf03c4ceca8a39076e6b7a0fb878e55534e2",
-  repo: "blog-comments",
-  owner: "clark-cui",
-  admin: ["clark-cui"],
-  id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
-  language: "zh-CN",
-  distractionFreeMode: true, // Facebook-like distraction free mode
-});
-
 onMounted(() => {
+  const gitalk = new Gitalk({
+    clientID: "a8430bf8a0464113ee24",
+    clientSecret: "df4cbf03c4ceca8a39076e6b7a0fb878e55534e2",
+    repo: "blog-comments",
+    owner: "clark-cui",
+    admin: ["clark-cui"],
+    id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
+    language: "zh-CN",
+    distractionFreeMode: true, // Facebook-like distraction free mode
+  });
   gitalk.render("gitalk-container");
 });
 </script>
-<style scoped>
-</style>
-
+<style scoped></style>
