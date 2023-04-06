ba750f108823f7a497f131ab50ca8a44e8bc64ac
clark-cui <cuirongchuan@163.com>
2023-04-07 00:19:13 +0800

    refector: pagination ui

--- .vitepress/theme/components/MyLayout.vue
+++ .vitepress/theme/components/MyLayout.vue
@@ -5,6 +5,9 @@
       <Category />
     </template>
     <template #doc-after>
+      <div>
+        <button @click="back">cd ··</button>
+      </div>
       <Comments />
     </template>
     <!-- Home slot-->
@@ -23,5 +26,34 @@ import Page from "./Page.vue";
 import Category from "./Category.vue";
 import Title from "./Title.vue";
 const { Layout } = DefaultTheme;
+const back = () => {
+  history.back();
+};
 </script>
-<style scoped></style>
+<style scoped>
+button {
+  display: inline-block;
+  position: relative;
+  color: var(--vp-c-color-d);
+  cursor: pointer;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+button::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  transform: scaleX(0);
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: var(--vp-c-color-d);
+  transform-origin: bottom right;
+  transition: transform 0.25s ease-out;
+}
+button:hover::after {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
+</style>
