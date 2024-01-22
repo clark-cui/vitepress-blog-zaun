27cebe5d4e8e7f8f8eba91b4de48416d34932daf
clark-cui <cuirongchuan@163.com>
2024-01-22 20:15:31 +0800

    chore: update deps

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import Archives from "./components/Archives.vue";
 import Tags from "./components/Tags.vue";
 import MyLayout from "./components/MyLayout.vue";
 import TwoSlashFloatingVue from "vitepress-plugin-twoslash/client";
+import "vitepress-plugin-twoslash/style.css";
 
 import "./custom.css";
 
